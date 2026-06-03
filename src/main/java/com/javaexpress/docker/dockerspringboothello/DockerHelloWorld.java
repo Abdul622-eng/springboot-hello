8c46c9b7f5736e15c0428daf54dd6e2187f0ed6d
Abdul622-eng <abdul.devops45@gmail.com>
2026-06-03 11:55:17 +0530

    Update DockerHelloWorld.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -1,16 +1,14 @@
 package com.javaexpress.docker.dockerspringboothello;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/docker")
-public class DockerHelloWorld {
+public class HelloController {
 
-	@GetMapping
-	public String getName() {
-		
-		return "Welcome to deployment in docker";
-	}
+    @GetMapping("/")
+    public String home() {
+        return "Hello DevOps! Application is running successfully 🚀";
+    }
 }
+``
