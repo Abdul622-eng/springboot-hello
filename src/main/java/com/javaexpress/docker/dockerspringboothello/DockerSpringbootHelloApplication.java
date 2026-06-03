db961bbd4192d6f4cf948ebd79ca70ca40f7615e
Abdul622-eng <abdul.devops45@gmail.com>
2026-06-03 11:52:59 +0530

    Update DockerSpringbootHelloApplication.java

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerSpringbootHelloApplication.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerSpringbootHelloApplication.java
@@ -1,13 +1,14 @@
 package com.javaexpress.docker.dockerspringboothello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DockerSpringbootHelloApplication {
+@RestController
+public class HelloController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerSpringbootHelloApplication.class, args);
-	}
-
+    @GetMapping("/")
+    public String home() {
+        return "Hello DevOps! Application is running successfully 🚀";
+    }
 }
+``
